0983c86542a5eea65cc505cb230dd6d6f6128b61
yokobond <koji.yokokawa@gmail.com>
2016-12-04 19:44:32 +0900

    Fix to display status ready.

--- utils.js
+++ utils.js
@@ -10,6 +10,18 @@
 
 (function (ext) {
 
+  // Cleanup function when the extension is unloaded
+  ext._shutdown = function () {
+  };
+
+
+  // Status reporting code
+  // Use this to report missing hardware, plugin or unsupported browser
+  ext._getStatus = function () {
+    return {status: 2, msg: 'Ready'};
+  };
+
+
   ext.match = function (str, regexp) {
     return str.match(regexp);
   };
